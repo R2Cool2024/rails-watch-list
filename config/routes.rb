10e392511447f43f2d713480f31298c2e06d7852
ran decool <ran.decool@gmail.com>
2024-11-14 17:01:49 +0100

    exo 2 etape 3 non terminée

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :lists do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  # get 'destroy', to: 
 end
